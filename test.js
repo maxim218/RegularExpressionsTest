c4a77c05dfdfe71332948e095d7a5d4df04fd35b
maxim <maxim218maxim218@yandex.ru>
2018-07-13 00:12:41 +0300

    Add search command

---
+++ test.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// поиск первого вхождения
+console.log(  "aabbccmaximeeff".search(/maxim/)  );  // 6
+console.log(  "aabbccmaximeeff".search(/nina/)   );  // -1
+console.log(  "aayyybbyyyccyyy".search(/yyy/)    );  // 2
+
+
